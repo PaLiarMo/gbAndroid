033f1201c1af5104aaa184b677b608e472dadf32
PaLiarMo <paliarmo@gmail.com>
2021-06-02 09:34:53 +0300

    homework N1

---
+++ app/src/main/java/com/example/uiexampleapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uiexampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_text_layout);
+    }
+}
